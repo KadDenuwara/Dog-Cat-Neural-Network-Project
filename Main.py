22dc467fea90940969feb38ec257093434363e2e
KadDenuwara <kad.denuwara@gmail.com>
2024-12-23 00:57:59 +0530

    Set main.py to run the model

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
+from keras.preprocessing import image
 
 # Load the trained model
 model = load_model('cat_dog_model.h5')
@@ -10,18 +10,27 @@ model = load_model('cat_dog_model.h5')
 # Function to predict
 def predict_image(image_path):
     # Load and preprocess the image
-    img = load_img(image_path, target_size=(150, 150))  # Resize to match input size
-    img_array = img_to_array(img) / 255.0  # Normalize
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    test_image = image.load_img(str(image_path), target_size=(64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
 
     # Predict
-    prediction = model.predict(img_array)
+    prediction = model.predict(test_image)
     class_label = 'Dog' if prediction[0][0] > 0.5 else 'Cat'
     return class_label
 
 
 # Test the function
 if __name__ == '__main__':
-    image_path = input("Enter the image path: ")
-    result = predict_image(image_path)
-    print(f'The image is a {result}.')
+
+    while True:
+        run = input("Do you want to run this model? y/n: ")
+        if run == 'y':
+            image_path = input("Enter the image path: ")
+            result = predict_image(image_path)
+            print(f'The image is a {result}.')
+
+        else:
+            break
+
+
